33bbc84b3a003016cd3b0394a4f7dc05caba6d8c
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-06-25 08:56:53 +0100

    styles applyed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to my first React App'
-  const likes = 60
-  const link = 'https://www.google.com/'
-  // const names = {names: 'ola', age:50}
+  
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>App Component</h1>
-        <p>{title}</p>
-        <p>We have {likes} likes</p>
-        {/* <p>{names}</p> */}
-        <p>{80}</p>
-        <p>{'hello Sandra'}</p>
-        <p>{[3,4,5,6]}</p>
-        <p>{Math.floor(Math.random()*5)+1} </p>
-        <a href={link}>Google Search</a>
+        <Home/>
       </div>
     </div>
   );
