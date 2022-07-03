ff50e715667de622eaef7c02d987dfaf6292994e
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-07-03 20:24:52 +0100

    created add new blog form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,26 @@
 const Create = () => {
-    return ( 
-        <div>my create route</div>
-     );
+    return (
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog Body</label>
+                <textarea
+                    required>
+                </textarea>
+                <label>Blog author</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+        </div>
+    );
 }
- 
+
 export default Create;
